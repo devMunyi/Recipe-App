30c454efaa099e3f82e3317a290585147af5588f
tarikbouari <tarikbouari@gmail.com>
2023-02-10 07:46:03 +0000

    Update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,8 @@ class RecipesController < ApplicationController
   before_action :recipe_id, only: %i[show destroy]
   before_action :user_id, only: %i[index destroy]
   before_action :recipe_obj, only: %i[show]
+  before_action :user_obj, only: %i[show]
+
 
   def index
     @recipes = Recipe.where(user_id: @user_id).order(id: :desc)
@@ -36,5 +38,9 @@ class RecipesController < ApplicationController
 
   def recipe_obj
     @recipe = Recipe.find(params[:id])
+  end 
+
+  def user_obj
+    @user = User.find(params[:user_id]) 
   end
 end
