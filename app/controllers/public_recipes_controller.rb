c031d62e66b8869b8270d282c2a83be2d8b229d2
tarikbouari <tarikbouari@gmail.com>
2023-02-10 07:46:46 +0000

    Update views layouts

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,4 @@
 class PublicRecipesController < ApplicationController
-  before_action :authenticate_user!, only: %i[index show destroy]
-
   def index
     @recipes = Recipe.where(public: true).order(id: :desc)
   end
